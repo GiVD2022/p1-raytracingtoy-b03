c4dabc1b75d85c894ab36ee0fa574342eb694bd3
ArturoLat <alatorca22@alumnes.ub.edu>
2023-03-24 17:18:55 +0100

    Actualitzacio Phong y BlinnPhong

--- Model/Rendering/BlinnPhongShading.cpp
+++ Model/Rendering/BlinnPhongShading.cpp
@@ -2,25 +2,37 @@
 
 
 vec3 BlinnPhongShading::shading(shared_ptr<Scene> scene, HitInfo& info, vec3 lookFrom,vector<shared_ptr<Light>> lights, vec3 globalLight) {
+    vec3 ambiental= vec3(0.0f);
+    vec3 difosa= vec3(0.0f);
+    vec3 specular = vec3(0.0f);
 
-    //Falta per acabar -> Zijian7
-    vec3 ambiental;
-    vec3 difosa;
-    vec3 specular;
-    vec3 N = normalize(info.normal);
-    vec3 L;
-    vec3 H;
-    vec3 V = lookFrom - info.p;
-    vec3 cosO;
-    float attenuation;
+    vec3 global = info.mat_ptr->Ka * globalLight;
+
     for(const shared_ptr<Light> &light: lights){
-        L = light->vectorL(info.p);
-        cosO = N*L;
-        H = normalize(L+V);
-        attenuation = light->attenuation(info.p);
-        ambiental = info.mat_ptr->Ka*light->getIa();
-        difosa = info.mat_ptr->Kd*light->getId()*cosO;
-        specular = info.mat_ptr->Ks*light->getIs()*H;
+        //Calcul ambiental
+        ambiental = info.mat_ptr->Ka * light->getIa();
+        //Asignem el vector L
+        vec3 L = light->vectorL(info.p);
+        //Vector Normal
+        vec3 N = info.normal;
+        //Angul entre L i N
+        float cos0 = dot(normalize(L),normalize(N));
+
+        //Calcul difosa
+        difosa = info.mat_ptr->Kd * light->getId() * cos0;
+
+        vec3 V = normalize(lookFrom - info.p);
+        vec3 H = normalize(L+V);
+        float cosb = dot(N, H);
+        float shi = info.mat_ptr->shininess;
+        float cos_b_exp = pow(cosb, shi);
+        specular = info.mat_ptr->Ks * light->getIs() * cos_b_exp;
+
+
+
+
+
+
     }
     //return ambiental;
     return difosa;
